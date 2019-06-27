4fc68287d678f39e7d71b195bbbc6ec5e3c1a266
negimohit1209 <mohitsngh1997@gmail.com>
2019-06-27 10:59:33 +0530

    add user model to the databse

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main() {
+import 'models/user.dart';
+import 'utils/database_helper.dart';
+
+List _users;
+void main() async {
+  var db = DatabaseHelper();
+  int savedUser = await db.saveUser(User("Mohit", "qwerty"));
+  print("User saved = $savedUser");
+  //all users
+  _users = await db.getAllUser();
   runApp(MaterialApp(title: 'database', home: Home()));
 }
 
