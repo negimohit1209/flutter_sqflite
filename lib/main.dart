772ea31d256f44250ef070f86d0c64b1192d3a8b
negimohit1209 <mohitsngh1997@gmail.com>
2019-06-27 12:46:43 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
@@ -9,10 +8,35 @@ import 'utils/database_helper.dart';
 List _users;
 void main() async {
   var db = DatabaseHelper();
-  int savedUser = await db.saveUser(User("Mohit", "qwerty"));
-  print("User saved = $savedUser");
+  // int savedUser = await db.saveUser(User("Raju", "sfasd"));
+  // print("User saved = $savedUser");
+  //get count
+  int count = await db.getCount();
+  print("Count: $count"); 
   //all users
-  _users = await db.getAllUser();
+  _users = await db.getAllUsers();
+  for(int i=0; i< _users.length; i++){
+    User user = User.map(_users[i]);
+    print("Username: ${user.username} ${user.id}");
+  }
+
+  //get a user
+  User user = await db.getUser(2);
+  print("username = ${user.username}");
+  User upadatedUser = User.fromMap({
+    "username" : "Mohit negi",
+    "password" : "changedpassword",
+    "id": 3
+  });
+
+  await db.updateUser(upadatedUser);
+
+  //delet a user 
+  //int user = await db.deleteUser(3);
+  
+  //update a user
+  
+
   runApp(MaterialApp(title: 'database', home: Home()));
 }
 
@@ -27,6 +51,20 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.lightGreen,
 
+      ),
+      body: ListView.builder(
+        itemCount: _users.length,
+        itemBuilder: (_, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            title: new Text("User: ${User.fromMap(_users[index]).username}"),
+            subtitle: Text("Id: ${User.fromMap(_users[index]).id}"),
+            onTap: () => debugPrint("${User.fromMap(_users[index]).password}"),
+          )
+        );
+       },
       ),
     );
   }
