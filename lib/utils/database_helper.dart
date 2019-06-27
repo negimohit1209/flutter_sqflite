e9f4663c6d87c19eeb1a37cda59a9855ea13937a
negimohit1209 <mohitsngh1997@gmail.com>
2019-06-27 11:30:02 +0530

    add DatabaseHelper class for CRUD functionality

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -1,31 +1,45 @@
-class User {
-  String _username;
-  String _password;
-  int _id;
-  User(this._username, this._password);
-  User.map(dynamic obj) {
-    this._username = obj['username'];
-    this._password = obj['password'];
-    this._id = obj['id'];
+import 'dart:async';
+import 'dart:io';
+
+import 'package:database_intro/models/user.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = new DatabaseHelper.internal();
+  final String tableUser = 'userTable';
+  final String columnId = "id";
+  final String columnUsername = 'username';
+  final String columnPassword = 'password';
+  factory DatabaseHelper() => _instance;
+  static Database _db;
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDb();
+    return _db;
   }
 
-  String get username => _username;
-  String get password => _password;
-  int get id => _id;
+  DatabaseHelper.internal();
 
-  Map<String, dynamic> toMap(){
-    var map = new Map<String, dynamic>();
-    map['username'] = _username;
-    map['password'] = _password;
-    if(id != null){
-      map['id'] = _id;
-    }
-    return map;
+  initDb() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "maindb.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDb;
+  }
+  void _onCreate(Database db, int version) async{
+    await db.execute(
+      "CREATE TABLE $tableUser($columnId INTEGER PRIMARY KEY, $columnUsername TEXT, $columnPassword TEXT)"
+    );
+  }
+
+  Future<int> saveUser(User user) async{ 
+    var dbClient = await db;
+    int res = await dbClient.insert(tableUser, user.toMap());
+    return res;
   }
   
-  User.fromMap(Map<String, dynamic> map){
-    this._username = map['username'];
-    this._password = map['password'];
-    this._id = map['id'];
-   }
 }
